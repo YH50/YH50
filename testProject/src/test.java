fce4d7120f7a0c21d75b9f2deb7b036421af65dd
DESKTOP-F4BC13L\hi <unijk4187@gmail.com>
2024-03-20 10:04:42 +0900

    first commit

---
+++ testProject/src/test.java
@@ -0,0 +1,9 @@
+public class test {
+
+    public static void main(String[] args){
+
+        System.out.println("2024년");
+        System.out.println("롯X 자이언츠");
+        System.out.print(" 가을야구 진출 기원");
+    }
+}
