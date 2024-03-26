7777a06102ea859bbf428888e99035e193aef7f1
DESKTOP-F4BC13L\hi <unijk4187@gmail.com>
2024-03-26 17:52:06 +0900

    240326 외출 내용

--- 01-lecture-source/chap05-array-lecture-source/src/com/ohgiraffers/section01/array/Application5.java
+++ 01-lecture-source/chap05-array-lecture-source/src/com/ohgiraffers/section01/array/Application5.java
@@ -2,6 +2,14 @@ package com.ohgiraffers.section01.array;
 
 public class Application5 {
 
+    public static void main(String[] args) {
 
+        /* 수업 목표 : 배열을 사용하는 예시를 이해하고 적용할 수 있다.*/
+        /* 임의의 카드를 한 장 뽑아서 출력하기*/
 
+        String[] cardNumbers = {"2", "4", "7", "10", "ACE", "KING", "JACK", "6", "8", "9", "QUEEN", "5"};
+        int randomNo = (int)(Math.random()*cardNumbers.length);
+        System.out.println(cardNumbers[randomNo]);
+
+    }
 }
