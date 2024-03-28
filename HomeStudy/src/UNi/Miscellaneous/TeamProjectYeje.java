eed9557abd20f373770877fd7b1ab2c91ffba6bf
UNiJang <163954979+YH50@users.noreply.github.com>
2024-03-28 18:20:52 +0900

    Update TeamProjectYeje.java

--- HomeStudy/src/UNi/Miscellaneous/TeamProjectYeje.java
+++ HomeStudy/src/UNi/Miscellaneous/TeamProjectYeje.java
@@ -4,20 +4,42 @@ import java.util.Scanner;
 
 public class TeamProjectYeje {
 
-    public void testSimpleSwitchStatement(){
+    public void yejeNextIfElseIfStatement(){
 
-        //정수 두개와 연산기호 모두 입력받아서 결과 출력하는 계산기 만들기
-
-        /* 정수 2개 & 연산 기호 입력하기 */
+        //볼 스피드 별로 등급 나누기
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("첫번째 정수 입력 : ");
-        int a = sc.nextInt();
-        System.out.print("두번째 정수 입력 : ");
-        int b = sc.nextInt();
+        System.out.print("선수 이름 입력 : ");
+        String name = sc.nextLine();
+        System.out.print("선수 구속 입력 : ");
+        int point = sc.nextInt();
+        String grade = "";
 
-        System.out.print("연산 기호 입력 : ");
-        char op = sc.next().charAt(0);
+        if(point >=150 && point <=160){
+            System.out.println("등급 : 국가대표");
+        }else if(point >=155){
+            System.out.println("등급 : 메이저리거");
+        }
+        if(point >= 140 && point < 150){
+            System.out.println("등급 : 주전");
+        }else if(point >=145) {
+            System.out.println("등급 : 에이스");
+        }
+        if(point >= 130 && point < 140){
+            System.out.println("등급 : 4-5선발");
+        }else if(point >=135) {
+            System.out.println("등급 : 레귤러");
+        }
+        else{
+             System.out.println("등급 : 퓨쳐스");
+        }
+
+      System.out.println(name + " 선수의 구속은 " + point + "km/h이고, 등급은 " + grade + "입니다.");
+      System.out.println("프로그램을 종료합니다.");
+   }
+
+
+        
 
 
 
