14bea9f53843c4fcd0bfb798451f4d33dcefdd08
UNiJang <163954979+YH50@users.noreply.github.com>
2024-04-01 22:56:16 +0900

    Update Product.java

--- 01-lecture-source/chap08-inheritance-lecture-source/src/com/ohgiraffers/section02/superkeyword/Product.java
+++ 01-lecture-source/chap08-inheritance-lecture-source/src/com/ohgiraffers/section02/superkeyword/Product.java
@@ -1,4 +1,32 @@
 package com.ohgiraffers.section02.superkeyword;
 
 public class Product {
+
+  private String code;  //상품코드
+  private String brand;  //제조사명
+  private String name;  //상품명
+  private int price;  //가격
+  private Date manufacturingDate;  //제조일자
+
+  public Product(){
+    System.out.println("Product 클래스 기본 생성자 호출함...");
+  }
+
+  // 변수 다 포함한 생성자 만들어주기
+
+  public Product(String code, String brand, String name, int price, Date ManufacturingDate){
+    this.code = code;
+    this.brand = brand;
+    this.name = name;
+    this.price = price;
+    this.manufacturingDate = manufacturingDate;
+    System.out.println("Product 클래스의 매개변수 있는 생성자 호출함...");
+  }
+// getter-setter 만들어주기
+  
+  
+
+
+    
+  
 }
